7ca87d077fb350d244681432d44911eda923cb2c
Abhishek Kumar <119113763+Abhikashu@users.noreply.github.com>
2022-12-12 22:58:01 +0530

    Create Class092-Join and Split_Method.py

---
+++ Class092-Join and Split_Method.py
@@ -0,0 +1,12 @@
+# split method
+user_info='harshit 24'.split()
+print(user_info)
+
+name,age='Abhishek,18'.split(",")
+print(name)
+print(age)
+
+
+#joint method
+user_info=['Abhishek','18']
+print(','.join(user_info))
