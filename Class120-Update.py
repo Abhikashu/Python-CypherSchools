8492c07a3b7dffc3bd0aa0ef09b914bac81fe5d7
Abhishek Kumar <119113763+Abhikashu@users.noreply.github.com>
2022-12-12 23:15:54 +0530

    Create Class120-Update.py

---
+++ Class120-Update.py
@@ -0,0 +1,7 @@
+user_info= {
+    'name': 'Abhishek',
+    'age' : '18',
+}
+more_info={'name' : 'Abhishek Kumar' , 'state' : 'U.P' , 'Hobbies' : ['coding' , 'reading']}
+user_info.update(more_info)
+print(user_info)
